68f7f5eab720b805a471265e93567f778ec25e56
Carles <carles.palaunavarro@yahoo.es>
2019-03-06 09:17:49 +0000

    Elimino echo innecesario

--- script5.sh
+++ script5.sh
@@ -4,7 +4,6 @@ read -p "Introduce valor: " variable1
 while [ $variable1 != 0 ]; do
     suma=`expr $suma + $variable1`
     contador=`expr $contador + 1`
-    echo "$suma $contador"
     read -p "Introduce valor: " variable1
 done
 media=`echo "$suma/$contador" | bc -l`
